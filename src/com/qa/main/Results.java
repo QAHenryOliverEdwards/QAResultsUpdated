e6d63b69d79530acb8a7f7365f74f3d84988a67b
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-01-07 10:01:06 +0000

    Added new functionality to check is student passed

--- src/com/qa/main/Results.java
+++ src/com/qa/main/Results.java
@@ -1,9 +1,9 @@
 package com.qa.main;
 
 public class Results {
-	int physics;
-	int chemistry;
-	int biology;
+	double physics;
+	double chemistry;
+	double biology;
 	double total;
 	float percentage;
 	
@@ -21,5 +21,36 @@ public class Results {
 	public float percentageCalc() {
 		float result = (float) (this.total/450);
 		return result*100;
+	}
+	
+	public boolean doPassSubject(double grade) {
+		double result = grade/150;
+		double percentResult = result * 100;
+		if (percentResult >= 60) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void doPassOverall() {
+		double biologyPercent = (this.biology/150)*100;
+		double chemistryPercent = (this.chemistry/150)*100;
+		double physicsPercent = (this.physics/150)*100;
+		double overallGrade = (biologyPercent + chemistryPercent + physicsPercent)/3;
+		System.out.println("Your overall grade is: " + overallGrade + "%"); 
+		
+		if (biologyPercent < 60) {
+			System.out.println("You failed because your biology grade was: " + biologyPercent + "%");
+		}
+		
+		if (chemistryPercent < 60) {
+			System.out.println("You failed because your biology grade was: " + chemistryPercent + "%");
+		}
+		
+		if (physicsPercent < 60) {
+			System.out.println("You failed because your biology grade was: " + physicsPercent + "%");
+		}
 	}
 }
--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -3,7 +3,13 @@ package com.qa.main;
 public class Runner {
 	public static void main(String[] args) {
 		Results student1 = new Results(20, 40, 60);
+		Results student2 = new Results(70, 40, 150);
+		Results student3 = new Results(115, 125, 150);
 		System.out.println(student1.getTotal());
 		System.out.println(student1.percentageCalc());
+		System.out.println(student1.doPassSubject(student2.biology));
+		student1.doPassOverall();
+		student2.doPassOverall();
+		student3.doPassOverall();
 	}
 }
